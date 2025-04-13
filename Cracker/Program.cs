35d350d42d4e50377f3b4d28caebc27e597b5905
杨中科 <yangzhongke8@gmail.com>
2025-04-13 12:25:30 +1200

    done

---
+++ Cracker/Program.cs
@@ -0,0 +1,73 @@
+using Microsoft.Data.Sqlite;
+using System.Reflection;
+
+namespace Cracker
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var asmMain = Assembly.LoadFrom("ZackDictCore.dll");
+            Thread thread = new Thread(() => {
+                MessageBox.Show("Wait");
+                try
+                {
+                    var form = Application.OpenForms[0];
+                    var fieldSqliteConnection = form.GetType().GetMembers(BindingFlags.Public | BindingFlags.NonPublic |
+                                            BindingFlags.Instance | BindingFlags.Static |
+                                            BindingFlags.DeclaredOnly)
+                                            .Where(e => e.MemberType == MemberTypes.Field)
+                         .Select(e => (FieldInfo)e).Where(e => e.FieldType.Name.Contains("SqliteConnection"))
+                                            .FirstOrDefault();
+                    var sqliteConnection = (SqliteConnection)fieldSqliteConnection.GetValue(form);
+                    sqliteConnection.Open();
+                    /*
+                    SqliteConnection destConnection = new SqliteConnection("Data Source=decrypted.db");
+                    destConnection.Open();
+                    sqliteConnection.BackupDatabase(destConnection);
+                    destConnection.Close();*/
+                    /*
+                    using var cmd = sqliteConnection.CreateCommand();
+                    cmd.CommandText = "SELECT name FROM sqlite_master WHERE type='table'";
+                    using var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string tableName = reader.GetString(0);
+                        MessageBox.Show(tableName);
+                    }*/
+                    /*
+                    using var cmd = sqliteConnection.CreateCommand();
+                    cmd.CommandText = "PRAGMA table_info(Words)";
+                    using var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string fieldName = reader.GetString(reader.GetOrdinal("name"));
+                        string type = reader.GetString(reader.GetOrdinal("type"));
+                        MessageBox.Show(fieldName+","+type);
+                    }*/
+                    using var cmd = sqliteConnection.CreateCommand();
+                    cmd.CommandText = "select word, chinese from Words";
+                    using var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string word = reader.GetString(0);
+                        string chinese = reader.GetString(1);
+                        MessageBox.Show(word + "," + chinese);
+                        return;
+                    }
+                    MessageBox.Show("Done");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            });
+            thread.Start();
+            asmMain.EntryPoint.Invoke(null, new object[0]);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# Hack_Sqlite
-A democase to show how to decrypt an .NET application that uses encrypted SQLite
+#Attempt 1: WinDbg
+1. Install WinDbg
+2. Load clr module(.Net Framework)
+```
+.loadby sos clr
+```
+3. Load clr module(.Net Core)
+- Install sos
+```
+dotnet tool install -g dotnet-sos
+dotnet sos install
+```
+- Load
+```
+.load C:\Users\{yourusername}\.dotnet\sos\sos.dll
+```
+4. Dump strings
+```
+!dumpheap -type System.String -strings
+```
+5. Search"Password"
+
+#Attempt 2: Edit dll
+1. Use ILSpy to understand Microsoft.Data.Sqlite.dll, let's hack Microsoft.Data.Sqlite.SqliteConnection.ConnectionString
+2. Regular way: add break point to set_ConnectionString
+3. My way:
+```
+ldarg.1
+ldc.i4.0
+new obj System.Void Microsoft.Data.Sqlite.SqliteException::.ctor(System.String, System.Int32)
+```
+4. Save
+5. Run. 
+
+#Attempt 3:
+1. Create a Windows Form application(.NET)
+2. Add reference to Microsoft.Data.Sqlite.dll under the cracked application
+3. 
+```
+var asmMain = Assembly.LoadFrom("ZackDictCore.dll");
+Thread thread = new Thread(() => {
+	MessageBox.Show("Wait");
+	try
+	{
+		var form = Application.OpenForms[0];
+		var fieldSqliteConnection = form.GetType().GetMembers(BindingFlags.Public | BindingFlags.NonPublic |
+								BindingFlags.Instance | BindingFlags.Static |
+								BindingFlags.DeclaredOnly)
+								.Where(e => e.MemberType == MemberTypes.Field)
+			 .Select(e => (FieldInfo)e).Where(e => e.FieldType.Name.Contains("SqliteConnection"))
+								.FirstOrDefault();
+		var sqliteConnection = (SqliteConnection)fieldSqliteConnection.GetValue(form);
+		sqliteConnection.Open();
+		SqliteConnection destConnection = new SqliteConnection("Data Source=decrypted.db");
+		destConnection.Open();
+		sqliteConnection.BackupDatabase(destConnection);
+		destConnection.Close();
+		MessageBox.Show("Done");
+	}
+	catch (Exception ex)
+	{
+		MessageBox.Show(ex.ToString());
+	}
+});
+thread.Start();
+asmMain.EntryPoint.Invoke(null, new object[0]);
+```
+4. Copy built into cracked application's dir
+5. Run(Where I made it back then). Oh my god
+6. Get table names
+7. Get table fields
+8. Run selected.
